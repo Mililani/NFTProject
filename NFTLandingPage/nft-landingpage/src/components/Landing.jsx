9b9d08d5d6ff4935f6bed871ab3d1d13d2694148
Mililani <mmrakhetsi00@gmail.com>
2023-02-10 01:38:20 +0200

    added p tag dummy info

--- NFTLandingPage/nft-landingpage/src/components/Landing.jsx
+++ NFTLandingPage/nft-landingpage/src/components/Landing.jsx
@@ -6,9 +6,19 @@ const Landing = () => {
             <div className="bg-blue-300 py-20 md:py-24">
                 <div className=" grid grid-cols-1 md:grid-cols-2 container mx-auto">
                 <div>
-                    <h1 className=" text-5xl text-center md:text-left md:text-8xl  text-white">
-                    Nft purchase <span>Buy Nft in bulk as time</span> in the world 
+                    <h1 className=" text-3xl text-center md:text-left md:text-6xl  text-white">
+                    Nft purchase{" "} 
+                    <span className="underline decoration-solid">
+                        Buy Nft in bulk as time
+                    </span>{" "} 
+                    in the {" "}
+                    <span className="underline decoration-solid">
+                    world
+                    </span>  
                     </h1>
+                    <p className="  md:text-2xl"> 
+                        theuhii iuiuyiuf ijiuuyy iuiuyiugog higyufugiugu iugiufugiui yuuuygiyg iufuy ut uyufutf  uguyfu yufuftu fufuf fyuff
+                    </p>
                 </div>
                 </div>
             </div>  
