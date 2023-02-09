7b5e433b4f64491c09c97a29f6be6c9e03b15bab
Mililani <mmrakhetsi00@gmail.com>
2023-02-10 01:07:13 +0200

    added landing page

--- NFTLandingPage/nft-landingpage/src/App.js
+++ NFTLandingPage/nft-landingpage/src/App.js
@@ -1,9 +1,11 @@
+import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="text-white">
      <Navbar/>
+     <Landing/>
     </div>
   );
 }
---
+++ NFTLandingPage/nft-landingpage/src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <>
+          <div className="bg-blue-300 py-20 md:py-24"></div>  
+        </>
+    );
+}
+
+export default Landing;
