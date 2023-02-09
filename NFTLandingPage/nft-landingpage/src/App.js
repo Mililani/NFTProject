3550e04cedc89f428ca68392c2ca395ad2a77857
Mililani <mmrakhetsi00@gmail.com>
2023-02-09 23:34:06 +0200

    navbar layout

--- NFTLandingPage/nft-landingpage/src/App.js
+++ NFTLandingPage/nft-landingpage/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="text-white">
-      app on
+     <Navbar/>
     </div>
   );
 }
--- NFTLandingPage/nft-landingpage/src/components/Navbar.jsx
+++ NFTLandingPage/nft-landingpage/src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="bg-blue-300 px-2 sm:px-2 py-2.5">
+            <div className="container flex flex-wrap items-center mx-auto justify-between">
+                <a href className=" text-3xl line-through">Nft</a>
+                 <div className="flex md:hidden md:order-2">
+                    <button type="button">
+                        add icon
+                    </button>
+                 </div>
+            </div>  
+        </nav>
     );
 }
 
