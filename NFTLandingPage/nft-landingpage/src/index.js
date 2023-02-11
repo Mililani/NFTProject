f4c4c05df140cf8febbb514cc289c9794608c161
Mililani <mmrakhetsi00@gmail.com>
2023-02-11 15:29:02 +0200

    imported ow carosuel

--- NFTLandingPage/nft-landingpage/src/index.js
+++ NFTLandingPage/nft-landingpage/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
