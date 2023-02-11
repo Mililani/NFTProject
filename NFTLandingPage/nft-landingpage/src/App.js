0f93d76b4f2277df59174fcc9b7139a673e04e0a
Mililani <mmrakhetsi00@gmail.com>
2023-02-11 15:52:14 +0200

    installed owl

--- NFTLandingPage/nft-landingpage/src/App.js
+++ NFTLandingPage/nft-landingpage/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="text-white">
+    <div className=" main text-white">
      <Navbar/>
      <Landing/>
     </div>
