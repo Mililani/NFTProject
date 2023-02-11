1397900c659730cdb83768015d673731b69a8f51
Mililani <mmrakhetsi00@gmail.com>
2023-02-11 15:08:26 +0200

    updated code edited

--- NFTLandingPage/nft-landingpage/src/components/Landing.jsx
+++ NFTLandingPage/nft-landingpage/src/components/Landing.jsx
@@ -4,56 +4,32 @@ import ima from "./images/—Pngtree—nft cyberpunk robot machine monster_88182
 
 const Landing = () => {
     return (
-        <>
-            <div className="bg-gradient-to-r from-blue-900 via-blue-900 to-gray-900 py-20 md:py-24  ">
-                <div className=" grid grid-cols-1 md:grid-cols-2 container mx-auto">
-                <div className='flex flex-col gap-4'>
-                    <h1 className=" text-3xl text-center md:text-left md:text-6xl  text-white">
-                    Discovery{" "} 
-                    <span className="underline decoration-solid">
-                    Collect & Sell
-                    </span>{" "} 
-                    <span className="text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-red-600">
-                        Extraodinary
-                    </span> {" "}
-                    <span className="underline decoration-solid">
-                    NFTs
-                    </span>  
-                    </h1>
-                    <p className="md:pt-2 px-2 md:text-2xl text-center  md:text-left "> 
-                        theuhii iuiuyiuf ijiuuyy iuiuyiugog higyufugiugu iugiufugiui yuuuygiyg iufuy ut uyufutf  uguyfu yufuftu fufuf fyuff
-                    </p>
-                    <div className="flex justify-between text-3xl text-center md:text-left md:text-6xl px-5 text-white" >
-                        <div className="text-white text-center">
-                            <h1 className="text-xl md:text-3xl">
-                                $5457
-                            </h1>
-                            <p className="text-sm md:text-base"> trade</p>
-                        </div>
-                        <div className="text-white text-center">
-                            <h1 className="text-xl md:text-3xl">
-                                1.3M
-                            </h1>
-                            <p className="text-sm md:text-base"> Total Users</p>
-                        </div>
-                        <div className="text-white text-center">
-                            <h1 className="text-xl md:text-3xl">
-                                940
-                            </h1>
-                            <p className="text-sm md:text-base"> Available NFTs</p>
-                        </div>
-                    </div>
-                    <div className=" w-3/ font-bold py-2 px-8 rounded text-3xl text-center md:text-center md:text-6xl">
-                    <button className=" bg-blue-400 hover:bg-blue-500  w-3/4 font-bold py-3 px-8 rounded text-3xl text-center md:text-center  text-white ">Mint</button>  
-                    </div>
-                </div>
-                    <div className="animate-bounce hidden md:block ">
-                    <img src={ima} alt="" />
-                    </div>
-                </div>
-            </div>  
-         
-        </>
+        <div className="lg:grid grid-cols-2 gap-3 items-center">
+        <div className="col-span-1 py-12 lg:py-0">
+          <h1 className="2xl:text-5xl lg:text-4xl sm:text-5xl text-4xl leading-snug 2xl:leading-snug sm:leading-snug lg:leading-snug font-head text-whites tracking-wide">
+            Discover, collect <br />
+            <span className="text-pink">and sell Timeless </span>
+            <span className="text-blue"> NFTs</span>
+          </h1>
+          <p className="font-medium text-lightBlack mt-3">
+            The world's first and largest digital marketplace for crypto
+            collectibles and non- fungible tokens (NFTs).
+          </p>
+          <div className="flex-colo w-full sm:w-3/5 border border-whites py-6 px-6 bg-subMain rounded mt-10">
+            <h2 className="italic font-semibold text-blue">
+              NFT drop coming soon!!
+            </h2>
+            <CountDown />
+          </div>
+        </div>
+        <div className="hero lg:block hidden">
+          <img
+            src="/images/main.png"
+            alt="Main"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
     );
 }
 
