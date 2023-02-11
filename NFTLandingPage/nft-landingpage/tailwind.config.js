14ddb1d95174ecbf079c2d8343e1fb4cda15a35f
Mililani <mmrakhetsi00@gmail.com>
2023-02-11 15:04:21 +0200

    updated model export

--- NFTLandingPage/nft-landingpage/tailwind.config.js
+++ NFTLandingPage/nft-landingpage/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#23073A",
+        subMain: "#502573",
+        subLightMain: "#7529B2",
+        whites: "#FFFFFF",
+        blacks: "#000000",
+        lightBlack: "#BCBCBC",
+        lightGray: "#F6F7FC",
+        pink: "#FD399B",
+        blue: "#3EAAE9",
+        overlay: "#23073ACC",
+      },
+      fontFamily: {
+        head: ["Russo One"],
+      },
+      screens: {
+        xs: "475px",
+      },
+    },
   },
   plugins: [],
-}
+};
