0424d13b1795fb4ace9b0f69056a805abe8a0bc3
Mililani <mmrakhetsi00@gmail.com>
2023-02-13 19:06:58 +0200

    update text

--- NFTLandingPage/nft-landingpage/src/components/Subs.jsx
+++ NFTLandingPage/nft-landingpage/src/components/Subs.jsx
@@ -20,7 +20,7 @@ function Subs() {
           </button>
         </div>
         <p className="font-medium text-sm text-center italic text-lightBlack mt-3">
-          By sending us your email you will get notfication when new NFT drops
+          By sending us your email you will get notfication when new NFTs drop
         </p>
         <div className="flex-rows mt-6 pb-12">
           <a href="https://github.com/Mililani" className={socialClass}>
